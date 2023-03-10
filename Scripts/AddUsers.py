c190817a2e54f61f02ee87508a0edc1b47cd6c91
Ryan Current <rjc7379@g.rit.edu>
2023-03-09 21:41:37 -0500

    Added User script and test data

---
+++ Scripts/AddUsers.py
@@ -0,0 +1,27 @@
+import psycopg2; 
+# Example credientials file, named .credentials
+# <username>
+# <password>
+
+# open credential file and read credentials
+f = open(".credentials", "r")
+
+username = f.readline()
+password = f.readline()
+
+# establishing a connection
+connection = psycopg2.connect(
+    database="p320_35", 
+    user=username, 
+    password=password, 
+    host='localhost', 
+    port='5432'
+)
+
+# setting auto commit false
+connection.autocommit = False
+
+# get a cursor object
+cursor = connection.cursor() 
+
+

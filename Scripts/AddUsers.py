8f187ecfd99783861e821d9ca531e0158bd32ded
Ryan Current <rjc7379@g.rit.edu>
2023-03-09 22:48:34 -0500

    Added script to add user data

--- Scripts/AddUsers.py
+++ Scripts/AddUsers.py
@@ -1,27 +1,44 @@
-import psycopg2; 
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
 # Example credientials file, named .credentials
 # <username>
 # <password>
 
 # open credential file and read credentials
-f = open(".credentials", "r")
+f = open('Scripts/.credentials', "r")
 
-username = f.readline()
+username = f.readline().replace("\n", "")
 password = f.readline()
 
-# establishing a connection
-connection = psycopg2.connect(
-    database="p320_35", 
-    user=username, 
-    password=password, 
-    host='localhost', 
-    port='5432'
-)
+try:
+    with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                            ssh_username=username,
+                            ssh_password=password,
+                            remote_bind_address=('localhost', 5432)) as server:
+        server.start()
+        print("SSH tunnel established")
+        params = {
+            'database': 'p320_35',
+            'user': username,
+            'password': password,
+            'host': 'localhost',
+            'port': server.local_bind_port
+        }
 
-# setting auto commit false
-connection.autocommit = False
 
-# get a cursor object
-cursor = connection.cursor() 
+        conn = psycopg2.connect(**params)
+        curs = conn.cursor()
+        print("Database connection established")
+        with open("Scripts/UserMockData.csv", "r") as dataFile:
+            for line in dataFile:
+                splitLine = line.split(",")
+                if(splitLine[1] < splitLine[6]): 
+                    splitLine[1] = splitLine[6]
+                curs.execute(f'INSERT INTO \"User\"(user_id, last_access_date, username, password, first_name, last_name, creation_date) VALUES ({splitLine[0]}, \'{splitLine[1]}\', \'{splitLine[2]}\', \'{splitLine[3]}\', \'{splitLine[4]}\', \'{splitLine[5]}\', \'{splitLine[6]}\')')
 
-edit
+        print("Successful inserted")
+        conn.commit()
+        conn.close()
+except Exception as exception:
+    print("Connection failed")
+    print(exception)

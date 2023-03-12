1965e292ddc2221fa13406252e5ca9784aae924c
Ryan Current <rjc7379@g.rit.edu>
2023-03-11 23:10:49 -0500

    Updated main app

--- Application/iMDBMSAPI.py
+++ Application/iMDBMSAPI.py
@@ -1,4 +1,96 @@
-# Console application to access the database 
+# PLEASE READ:
+#
+# This will be the file that run the main function. 
+# Avoid adding code here, create another file with 
+# functions that return SQL code. Edit this file only
+# to call your functions (or if there is a bug). 
+#
+
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
+from UserCommands import *
+
+
+# connects to the server and runs commands 
+def main():
+    try:
+        with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                ssh_username=username,
+                                ssh_password=password,
+                                remote_bind_address=('localhost', 5432)) as server:
+            server.start()
+            print("SSH tunnel established")
+            params = {
+                'database': 'p320_35',
+                'user': username,
+                'password': password,
+                'host': 'localhost',
+                'port': server.local_bind_port
+            }
+
+
+            conn = psycopg2.connect(**params)
+            curs = conn.cursor()
+
+            # This loop will run
+            # conn and curs are passed so that you can run SQL commands 
+            while True:
+                result = runCommand(conn, curs)
+                if result == 'stop':
+                    break
+
+            conn.close()
+    except Exception as exception:
+        print("Connection failed")
+        print(exception)
+        
+        
+# reads from the console, runs the correct code
+#
+# console input will be as follows 
+#
+# [command] [arg1] [arg2] [arg3]
+# 
+# write your function calls in this function
+def runCommand(conn, curs):
+    # read user input
+    input_line = input('iMDBMS command >> ')
+    # split line on spaces 
+    split_line = input_line.split(' ')
+
+    # parse the input
+    # convert everything to lowercase 
+    command = split_line[0].lower()
+    args = [arg.lower() for arg in split_line[1:]]
+
+    # ============== QUICK START INFO ==========================
+    #
+    # call your functions using the switch statment below, 
+    # make up a command, *add it to the displayCommands function*
+    # 
+    # at this point in the code you have 
+    # conn - used to execute SQL commands
+    # curs - used to execute SQL commands 
+    # command - the command typed in by the user (lowercase)
+    # args - a string array of args typed by the user (lowercase)
+
+    # =============== STUDENT CODE BLOCK STARTS HERE ============
+    # =============== (obviously im joking) =====================
+    # =============== (this was all written by a student) =======
+
+    match command:
+        # add your commands here
+        case 'list':
+            displayCommands()
+        case 'help':
+            displayUsage()
+        case 'stop':
+            return 'stop' 
+        # default 
+        case _:
+            displayUsage()
+
+    # =============== STUDENT CODE BLOCK ENDS HERE ===============
 
 
 def displayUsage():
@@ -14,44 +106,6 @@ def displayCommands():
 
     # add more commands here
     print()
-
-
-# reads from the console, runs the correct code
-#
-# console input will be as follows 
-#
-# [command] [arg1] [arg2] [arg3]
-# 
-def main():
-    while True: 
-        # read user input
-        input_line = input('iMDBMS command >> ')
-        # split line on spaces 
-        split_line = input_line.split(' ')
-
-        # parse the input
-        # convert everything to lowercase 
-        command = split_line[0].lower()
-        args = [arg.lower() for arg in split_line[1:]]
-
-        match command:
-
-            case 'list':
-                displayCommands()
-            case 'help':
-                displayUsage()
-            case 'stop':
-                break 
-            # default 
-            case _:
-                displayUsage()
-        
-        
-
-
-
-
-
 
 
 if __name__ == "__main__":
